9afe344377e9e1fa442c60a403e406dd3e69f799
zhanghn <notre1024@163.com>
2026-04-30 21:16:08 +0800

    config(env): 更新配置文件中的端口和服务地址
    - 将 VITE_SOCKET_URL 从 9092 端口更改为 9093 端口
    - 将 application.yml 中的活动配置文件从 frp 更改为 lan
    - 将服务端口从 8024 更改为 8025
    - 移除 application.yml 中的 system 配置块
    - 在 application-frp.yml 和 application-lan.yml 中添加 socketio 端口为 9093
    - 在 application-lan.yml 中添加 Windows 路径格式的系统配置
    - 更新 vite.config.ts 中代理目标端口为 8025

--- slackjar-react-admin/vite.config.ts
+++ slackjar-react-admin/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8024/slack',
+                target: 'http://127.0.0.1:8025/slack',
                 // target: 'http://106.54.26.72:8025/slack',
                 // target: 'http://192.168.1.110:8025/slack',
                 changeOrigin: true,
